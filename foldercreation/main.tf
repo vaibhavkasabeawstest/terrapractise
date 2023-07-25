10790b4c023f887e9d7839668a682128953bf086
vaibhav <vaibhavkasabeawstest@gmail.com>
2023-07-25 08:00:39 +0530

    File creating task

---
+++ foldercreation/main.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_providers {
+    local = {
+      source = "hashicorp/aws"
+      version = "5.9.0"
+    }
+  }
+}
+
+provider "local" {
+
+}
+
+resource "local_file" "vk_text1" {
+    filename                                = "/temp/terracode/vk.txt"
+    content                                 = "Hi Omprakash this folder creation using terrafor"
+}
