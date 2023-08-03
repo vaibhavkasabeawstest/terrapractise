1afa1ed247892326e3afb49769d16be6a2e686bd
vaibhav <vaibhavkasabeawstest@gmail.com>
2023-08-03 09:00:09 +0530

    filecreation

---
+++ FolderCreation1/main.tf
@@ -0,0 +1,16 @@
+
+terraform {
+  required_providers {
+    local = {
+      source = "hashicorp/local"
+      version = "2.4.0"
+    }
+  }
+}
+
+provider "local" {
+  # Configuration options
+}
+resource "local_file" "om_text1" {
+    filename                                = "/temp/terracode/om.txt"
+    content                                 = "Hi Omprakash this folder creation using terraform"
